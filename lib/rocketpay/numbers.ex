b56eec4c11c3ce1114fab1b7c4c786ff07adae25
jv <joaovictor.bispo@novaescola.org.br>
2021-02-22 21:42:38 -0300

    app created and numbers

---
+++ lib/rocketpay/numbers.ex
@@ -0,0 +1,16 @@
+defmodule Rocketpay.Numbers do
+  def sum_from_file (filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, result}) do
+    result = String.split(result, ",")
+    result = Stream.map(result, fn number -> String.to_integer(number) end)
+    result = Enum.sum(result)
+    {:ok, %{result: result}}
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "invalid file"}}
+end
